1d77f31ba38a6d55a0464511ab393c98012e5c3e
Baartooo <bartek.gajos19@gmail.com>
2021-03-02 22:31:13 +0100

    initialized about content

--- src/components/+about/About.component.tsx
+++ src/components/+about/About.component.tsx
@@ -7,7 +7,7 @@ import s from './About.module.scss';
 
 interface IAbout {
   background: FixedObject;
-  header: string
+  header: string;
 }
 
 export const About: FC<IAbout> = ({ background, header }) => {
@@ -23,6 +23,30 @@ export const About: FC<IAbout> = ({ background, header }) => {
           }}
         />
         <div className={s.about__backgroundOverlay} />
+      </div>
+      <div className={s.about__content}>
+
+        <div className={s.about__explanation}>
+          <h2 className={s.about__title}>Kim jesteśmy</h2>
+          <p className={s.about__paragraph}>
+            Zajmujemy się <span className={s.about__bold}>obsługą firm pod względem BHP, prowadzeniem szkoleń BHP i ochroną przeciwpożarową.</span> Dysponujemy
+            profesjonalną, wykształconą kadrą, posiadającą <span className={s.about__bold}>odpowiednie kwalifikacje i uprawnienia</span>,
+            co gwarantuje
+            <span className={s.about__bold}>sumienne i profesjonalne wykonanie usługi</span>. Zajmujemy się również
+            obsługą branży budowlanej.
+          </p>
+          <p className={s.about__paragraph}>
+            Jesteśmy zaangażowani w działania na rzecz
+            <span className={s.about__bold}>poprawy bezpieczeństwa pracy</span>.
+          </p>
+          <p className={s.about__paragraph}>
+            Naszym celem jest <span className={s.about__bold}>promowanie najlepszych praktyk w zakresie bezpieczeństwa i ochrony pracy</span>,
+            w tym
+            inspirowanie
+            pracodawców do tworzenia <span className={s.about__bold}>bezpiecznych i ergonomicznych miejsc pracy</span>.
+          </p>
+        </div>
+
       </div>
 
 
