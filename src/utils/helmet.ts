3c941304fc1d1480b575a99ec4befce08b0be221
Baartooo <bartek.gajos19@gmail.com>
2021-03-10 20:26:26 +0100

    share image

--- src/utils/helmet.ts
+++ src/utils/helmet.ts
@@ -1,4 +1,5 @@
 import favicon from 'assets/images/favicon.png';
+import share from 'assets/images/share.jpg';
 
 const title = 'Agat BHP';
 const description = 'Kompleksowa obsługa BHP i PPOŻ';
@@ -13,6 +14,11 @@ export const helmet = {
       name: 'viewport',
       content: 'width=device-width, initial-scale=1, user-scalable=no',
     },
+    { property: 'og:title', content: title },
+    { property: 'og:image', content: share },
+    { property: 'og:image:width', content: '800px' },
+    { property: 'og:image:height', content: '440px' },
+    { property: 'og:image:alt', content: description },
     { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
     { name: 'msapplication-navbutton-color', content: '#000' },
     { name: 'msapplication-TileColor', content: '#000' },
